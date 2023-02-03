29a41e0382ceb10691dbc0d91455741ea1162d0f
falcogne <53317641+falcogne@users.noreply.github.com>
2023-02-03 16:24:21 -0600

    little bit of network setup
    still have to decide whether to set up connections forwards or backwards but at least got the layers being made

--- structure.py
+++ structure.py
@@ -1,23 +1,32 @@
 # slow down there bud, you're already lost
 
 class Network():
-    pass
+    def __init__(self, nodes):
+        self.layers = [InputLayer(nodes[0])]
+        for num in nodes[1:]:
+            layers.append(Layer(num))
 
+        # now have layers with nodes in them but no connections between them
+        # and no input/output layer
+
+        for i in range(len(self.layers)-1, 0, -1): # go backwards through list of layers - don't include input layer
+            layer[i].connect(layer[i-1])
 
 class Layer():
-    def __init__(self, prevLayer, numNodes):
+    def __init__(self, numNodes):
         self.nodes = []
         for _ in range(numNodes):
             newNode = Node()
-            newNode.connect(prevLayer)
             self.nodes.append(newNode)
 
+class InputLayer(Layer):
+    def __init__(self, inputVector):
+        pass
 
 class Node():
     def connect(self, layer):
         for node in layer.nodes:
             self.connections.append(Connection(self, node))
-
 
 class Connection():
     pass
